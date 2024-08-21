4859c4b1507b6b6d94803cc3a9e786e0f1b76491
DenisPepper <thech@yandex.ru>
2024-08-21 19:40:04 +0300

    Добавит обработчик на нажатие Enter

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ const app = {
     },
     methods: {
         handleInputChange(evt) {
-           this.value = evt.target.value;
+            this.value = evt.target.value;
         },
         handleButtonClick() {
+            if (!this.value) return;
             this.notes.push(this.value)
             this.value = ''
+        },
+        handleKeyPress(evt) {
+            if(evt.key === "Enter") {
+                evt.preventDefault();
+                this.handleButtonClick();
+            }
+
         }
     }
 };
